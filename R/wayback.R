17b7a34d04c35868a1484bcf44e4b8b8fb0aa7d0
jsta <jstachelek@utexas.edu>
2016-04-18 08:35:04 -0400

    add timestamp argument

--- R/wayback.R
+++ R/wayback.R
@@ -1,13 +1,16 @@
 #'@name wayback
 #'@param url character url
-#'@examples wayback("example.com")
-wayback <- function(url){
-	wayback_GET(wayback_base(), url)
+#'@param timestamp character date of 1-14 digits in YYYYMMDDhhmmss format
+#'@examples 
+#'wayback(url = "example.com", timestamp = "20060101")
+#'wayback(url = "example.com")
+wayback <- function(url, timestamp = NULL){
+	args <- list(url = url, timestamp = timestamp)
+	wayback_GET(wayback_base(), args)
 }
 
 wayback_GET <- function(base_url, args, ...) {
-	query <- setNames(list(args), c("url"))
-	res <- httr::GET(base_url, query = query, ...)
+	res <- httr::GET(base_url, query = args, ...)
 	httr::stop_for_status(res)
 	jsonlite::fromJSON(httr::content(res, "text"))
 }

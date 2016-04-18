88efc4bbc909ccedfb49a22272d87a9b7da2a1e6
jsta <jstachelek@utexas.edu>
2016-04-18 08:20:13 -0400

    working wayback query

--- R/wayback.R
+++ R/wayback.R
@@ -1,21 +1,12 @@
 
 wayback_base <- function() "http://archive.org/wayback/available"
 
-
 #'@name wayback_GET
+#'@example wayback_GET(wayback_base(), "example.com")
 wayback_GET <- function(url, args, ...) {
-	res <- httr::GET(url, query = args, ...)
+	query <- setNames( list(args), c("url"))
+	res <- httr::GET(url, query = query, ...)
 	httr::stop_for_status(res)
-	browser()
-	
-	httr::content(res, "text")
-	url <- httr::modify_url(url, query = args)
-	jsonlite::fromJSON(url)
-	
-	
-	httr::headers(res)
-	#jsonlite::fromJSON(res)
-	#httr::accept_json(res)
 	jsonlite::fromJSON(httr::content(res, "text"))
 }
 
